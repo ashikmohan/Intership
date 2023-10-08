a16606301063a2c652ad8583a91d9e5fd52f3473
Ashik Mohan <ashikmohan2000@gmail.com>
2023-10-08 21:37:10 +0530

    change

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const path =require('path');
 
 app.use(express.static(path.join(__dirname,'frontend')))
 
-app.get('/*',async(req,res)=>{
+app.get('*',async(req,res)=>{
     res.sendFile(path.join(__dirname,'frontend','index.html'))
 });
 
